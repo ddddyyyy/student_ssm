37f76c3b4e836288e2bdb767c512e8ceb9c8cae2
ddddyyyy <2115601599@qq.com>
2018-11-03 22:39:35 +0800

    ,.,

--- src/main/java/com/mdy/student/web/student/StudentController.java
+++ src/main/java/com/mdy/student/web/student/StudentController.java
@@ -4,6 +4,7 @@ import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
